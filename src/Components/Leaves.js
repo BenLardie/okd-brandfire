4f234dd2610db549be001d9bf8a45ebefbf89149
Ben Lardie <benlardie@gmail.com>
2021-01-11 16:42:02 -0500

    all items are on layout except form text

--- src/Components/Leaves.js
+++ src/Components/Leaves.js
@@ -3,13 +3,15 @@ import '../css/leaves.css'
 import leafOne from '../images/leaf1.png'
 import leafTwo from '../images/leaf2.png'
 import leafPile from '../images/leafpile.png'
+import plate from '../images/plate.png'
 
 export default function Leaves() {
     return (
         <>
-            <img src={leafOne} className='leaf-one' />
-            <img src={leafTwo} className='leaf-two' />
-            <img src={leafPile} className='leaf-pile' />
+            <img src={leafOne} className='leaf-one' alt='leaf' />
+            <img src={leafTwo} className='leaf-two' alt='leaf' />
+            <img src={leafPile} className='leaf-pile' alt='leaf' />
+            <img src={plate} className='plate' alt='plate' />
         </>
     )
 }
