ff269daf886e23218de74f598acdf8c0be275352
Ben Lardie <benlardie@gmail.com>
2021-01-12 12:48:30 -0500

    responsive looks good so now its time to get functonal

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/nav.css'
 import canadaDry from '../images/canadadry.png'
 
 export default function Nav() {
     let language = 'eng'
     let changeLanguage = () => {}
+    const [navState, setNavState] = useState('')
+    const navClick = () => {
+        if (navState === '') {
+            setNavState('open')
+            // setNavOpen('navOpen')
+        } else {
+            setNavState('')
+            // setNavOpen('')
+        }
+    }
     return (
         <nav>
-            <img src={canadaDry} />
+            <img src={canadaDry} className='logo' alt='logo' />
             <div className='buttons'>
-            <a href='#'>recipe download</a>
+            <a href='#' className='recipe'>recipe download</a>
             <select
                 value={language}
                 onChange={changeLanguage}
+                className='language-select'
             >
                 <option value="english">English</option>
                 <option value="french">French</option>
             </select>
+            </div>
+            <div className='hamburger'>
+                <i className="fa fa-bars"></i>
             </div>
         </nav>
     )
